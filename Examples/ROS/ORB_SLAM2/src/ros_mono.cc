dcd517c82042eac6435584b68bb9be2733231604
Randy Mackay <rmackay9@yahoo.com>
2017-03-27 15:36:35 +0900

    ros_mono: minor formatting fixes

--- Examples/ROS/ORB_SLAM2/src/ros_mono.cc
+++ Examples/ROS/ORB_SLAM2/src/ros_mono.cc
@@ -122,17 +122,16 @@ void ImageGrabber::GrabImage(const sensor_msgs::ImageConstPtr& msg)
 
 
     /* transform into global right handed coordinate system, publish in ROS*/
-    tf::Matrix3x3 cameraRotation_rh(  - world_lh.at<float>(0,0),   world_lh.at<float>(0,1),   world_lh.at<float>(0,2),
-                                  - world_lh.at<float>(1,0),   world_lh.at<float>(1,1),   world_lh.at<float>(1,2),
-                                    world_lh.at<float>(2,0), - world_lh.at<float>(2,1), - world_lh.at<float>(2,2));
+    tf::Matrix3x3 cameraRotation_rh(-world_lh.at<float>(0,0),  world_lh.at<float>(0,1),  world_lh.at<float>(0,2),
+                                    -world_lh.at<float>(1,0),  world_lh.at<float>(1,1),  world_lh.at<float>(1,2),
+                                     world_lh.at<float>(2,0), -world_lh.at<float>(2,1), -world_lh.at<float>(2,2));
 
-    tf::Vector3 cameraTranslation_rh( world_lh.at<float>(0,3),world_lh.at<float>(1,3), - world_lh.at<float>(2,3) );
+    tf::Vector3 cameraTranslation_rh(world_lh.at<float>(0,3), world_lh.at<float>(1,3), -world_lh.at<float>(2,3));
 
     //rotate 270deg about x and 270deg about x to get ENU: x forward, y left, z up
-    const tf::Matrix3x3 rotation270degXZ(   0, 1, 0,
-                                            0, 0, 1,
-                                            1, 0, 0);
-
+    const tf::Matrix3x3 rotation270degXZ(0, 1, 0,
+                                         0, 0, 1,
+                                         1, 0, 0);
 
     tf::Matrix3x3 globalRotation_rh = cameraRotation_rh * rotation270degXZ;
     tf::Vector3 globalTranslation_rh = cameraTranslation_rh * rotation270degXZ;
